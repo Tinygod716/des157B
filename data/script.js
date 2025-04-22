20feb17bb170533ce65808b36e41975203406bc4
tinygod716 <qawertgyuiolp@gmail.com>
2025-04-22 01:34:44 -0700

    index.html

---
+++ data/script.js
@@ -0,0 +1,33 @@
+(function () {
+    'use strict';
+    let counter = 0;
+    const number = document.getElementById("number");
+    const circle = document.getElementById("progress-circle");
+
+let interval = setInterval(() => {
+  if (counter == 65) {
+    clearInterval(interval);
+  } else {
+    counter++;
+    number.innerHTML = `${counter}%`;
+
+    const offset = 440 - (440 * counter) / 100;
+    circle.style.strokeDashoffset = offset;
+  }
+}, 30);
+
+//different color for h1
+
+const colors = ['red', 'orange', 'yellow', 'black', 'blue', 'purple', 'pink'];
+const title = document.getElementById('colorful-title');
+const text = title.innerText;
+let newHTML = '';
+
+for (let i = 0; i < text.length; i++) {
+  const color = colors[i % colors.length];
+  const char = text[i] === ' ' ? '&nbsp;' : text[i];
+  newHTML += `<span style="color: ${color};">${char}</span>`;
+}
+
+title.innerHTML = newHTML;
+})();
