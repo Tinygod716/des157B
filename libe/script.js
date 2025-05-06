000bd916b24230b6fe47c03ac013beb945e4f7b2
tinygod716 <qawertgyuiolp@gmail.com>
2025-05-06 00:29:16 -0700

    index.html

--- libe/script.js
+++ libe/script.js
@@ -3,14 +3,16 @@ function modifyHeaderAndControlScroll() {
     document.body.style.overflow = 'hidden';
 
     setTimeout(() => {
+        const OpenText = document.querySelector('.opening p');
+        OpenText.classList.add('fade-out');
+    }, 3000);
+
+    setTimeout(() => {
         // Change header background color
         const Header = document.querySelector('header');
-        const OpenText = document.querySelector('.opening p');
         const Video = document.createElement('video');
         const ScrollText = document.querySelector('footer p');
         const ScrollBox = document.querySelector('.scrollanimate');
-
-        OpenText.classList.add('fade-out');
 
         Video.src = 'media/opening.mp4';
         Video.autoplay = true;
@@ -26,7 +28,7 @@ function modifyHeaderAndControlScroll() {
         document.body.style.overflow = '';
         ScrollText.style.display = 'block';
         ScrollBox.style.display = 'block';
-    }, 3000); 
+    }, 4000); 
 }
 
 modifyHeaderAndControlScroll();
